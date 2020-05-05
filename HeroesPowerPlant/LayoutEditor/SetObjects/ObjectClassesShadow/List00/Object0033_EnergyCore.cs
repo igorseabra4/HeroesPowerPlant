c2fd81c6055702d62011e0c427982d40a6a37f55
dreamsyntax <dreamsyntax@gmail.com>
2020-05-05 00:20:39 -0700

    shadow misc bytes for LightColli, Fan, Searchlight, EnergyCore, Balloon

---
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesShadow/List00/Object0033_EnergyCore.cs
@@ -0,0 +1,9 @@
+﻿namespace HeroesPowerPlant.LayoutEditor {
+    public class Object0033_EnergyCore : SetObjectShadow {
+        public EnergyCoreType CoreType {
+            get => (EnergyCoreType)ReadInt(0);
+            set => Write(0, (int)value);
+        }
+    }
+}
+
---
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesShadow/List25/Object2594_Fan.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+
+namespace HeroesPowerPlant.LayoutEditor {
+    public class Object2594_Fan : SetObjectShadow {
+
+        public FanType FanType { //0 or 1
+            get => (FanType)ReadInt(0);
+            set => Write(0, (int)value);
+        }
+
+        public FanForm FanForm { //0
+            get => (FanForm)ReadInt(4);
+            set => Write(4, (int)value);
+        }
+
+        public float Radius {
+            get => ReadFloat(8);
+            set => Write(8, value);
+        }
+
+        [Description("Only for FanForm BoxType")]
+        public float BoxTypeAirHeight { //always 0
+            get => ReadFloat(12);
+            set => Write(12, value);
+        }
+
+        [Description("Cylinder Type Air Height; Box Type Radius")]
+        public float AirHeightANDBoxTypeRadius {
+            get => ReadFloat(16);
+            set => Write(16, value);
+        }
+
+        public float AirStrength {
+            get => ReadFloat(20);
+            set => Write(20, value);
+        }
+
+        public float TimeToRun {
+            get => ReadFloat(24);
+            set => Write(24, value);
+        }
+
+        public float TimeToRecharge {
+            get => ReadFloat(28);
+            set => Write(28, value);
+        }
+
+        public CommonNoYes HasModel { //0 or 1
+            get => (CommonNoYes)ReadInt(32);
+            set => Write(32, (int)value);
+        }
+
+        public FanRunning FanRunning { //-1 or 255
+            get => (FanRunning)ReadInt(36);
+            set => Write(36, (int)value);
+        }
+    }
+
+    public enum FanType {
+        UpperWay,
+        SideWay
+    }
+
+    public enum FanForm {
+        Cylinder,
+        Box,
+    }
+
+    public enum FanRunning {
+        Yes=-1,
+        No=255
+    }
+}
+
