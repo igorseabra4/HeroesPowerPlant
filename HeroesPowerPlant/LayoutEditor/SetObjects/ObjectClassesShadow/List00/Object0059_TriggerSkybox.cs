344d24957125694c27703a5117c8a167eecdfc96
dreamsyntax <dreamsyntax@gmail.com>
2020-05-16 23:25:07 -0700

    define Debug objects, label env weapons, TriggerSkybox misc bytes

--- HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesShadow/List00/Object0059_TriggerSkybox.cs
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesShadow/List00/Object0059_TriggerSkybox.cs
@@ -2,6 +2,9 @@
 
 namespace HeroesPowerPlant.LayoutEditor {
     public class Object0059_TriggerSkybox : SetObjectShadow {
+        //BlockChange (init only, no exec())
+        //Player::Player, Player::PlayerObject,
+        //Player::PlayerBase, Type, On/Off, Radius
 
         // 0 unk (always 0)
         // 1 int
@@ -11,34 +14,50 @@ namespace HeroesPowerPlant.LayoutEditor {
         // 5 int
         // 6 int
 
-        [Description("Always 0 in original objects, purpose unknown")]
-        public int Unknown0 {
+        [Description("Always 0 in original objects, purpose unknown\n Insert an int here for testing")]
+        public int Unknown_int0 {
             get => ReadInt(0);
             set => Write(0, value);
         }
-        public int int1 {
-            get => ReadInt(4);
-            set => Write(4, value);
+
+        [Description("Always 0 in original objects, purpose unknown\n Insert a float here for testing")]
+        public float Unknown_float0 {
+            get => ReadFloat(0);
+            set => Write(0, value);
         }
 
-        public float float2 {
+        [Description("Force the chunk(s) On or Off while in Radius.\n Ignores Visibility constraints")]
+        public CommonNoYes Draw {
+            get => (CommonNoYes)ReadInt(4);
+            set => Write(4, (int)value);
+        }
+
+        [Description("Radius from origin that the object is active")]
+        public float Radius {
             get => ReadFloat(8);
             set => Write(8, value);
         }
 
-        public int int3 {
+        [Description("Chunk to force on/off, set to -1 or 0 to pick none")]
+        public int Chunk_1 {
             get => ReadInt(12);
             set => Write(12, value);
         }
-        public int int4 {
+
+        [Description("Chunk to force on/off, set to -1 or 0 to pick none")]
+        public int Chunk_2 {
             get => ReadInt(16);
             set => Write(16, value);
         }
-        public int int5 {
+
+        [Description("Chunk to force on/off, set to -1 or 0 to pick none")]
+        public int Chunk_3 {
             get => ReadInt(20);
             set => Write(20, value);
         }
-        public int int6 {
+
+        [Description("Chunk to force on/off, set to -1 or 0 to pick none")]
+        public int Chunk_4 {
             get => ReadInt(24);
             set => Write(24, value);
         }
---
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesShadow/List25/Object2592_DebugMissionClearCollision.cs
@@ -0,0 +1,17 @@
+﻿namespace HeroesPowerPlant.LayoutEditor {
+    public class Object2592_DebugMissionClearCollision : SetObjectShadow {
+
+        public DebugClearMissionType MissionType {
+            get => (DebugClearMissionType)ReadInt(0);
+            set => Write(0, (int)value);
+        }
+    }
+
+    public enum DebugClearMissionType {
+        Dark,
+        Normal,
+        Hero,
+        Boss
+    }
+}
+
