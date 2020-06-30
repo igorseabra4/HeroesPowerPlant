c8f1f1354daed520f9c55ae83e3e9348f6e411a1
Muzzarino <dvdresources@gmail.com>
2020-06-29 21:19:15 -0700

    Update Object0999_Powder.cs

--- HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesHeroes/List09/Object0999_Powder.cs
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesHeroes/List09/Object0999_Powder.cs
@@ -1,7 +1,49 @@
-﻿namespace HeroesPowerPlant.LayoutEditor
+using SharpDX;
+
+namespace HeroesPowerPlant.LayoutEditor
 {
     public class Object0999_Powder : SetObjectHeroes
     {
+    
+        public override void Draw(SharpRenderer renderer)
+        {
+            if (models != null && renderer.dffRenderer.DFFModels.ContainsKey(ModelNames[(int)ModelNumber][0]))
+            {
+                SetRendererStates(renderer);
+                renderData.worldViewProjection = transformMatrix * renderer.viewProjection;
+
+                renderer.Device.SetBlendStateAdditive();
+                renderer.Device.SetCullModeNone();
+                renderer.Device.ApplyRasterState();
+                renderer.Device.UpdateAllStates();
+
+                renderer.Device.UpdateData(renderer.tintedBuffer, renderData);
+                renderer.Device.DeviceContext.VertexShader.SetConstantBuffer(0, renderer.tintedBuffer);
+
+                if (renderer.dffRenderer.DFFModels[ModelNames[(int)ModelNumber][0]] != null)
+                    renderer.dffRenderer.DFFModels[ModelNames[(int)ModelNumber][0]].Render(renderer.Device);
+            }
+
+            else
+            {
+                renderData.Color = isSelected ? renderer.selectedColor : renderer.normalColor;
+
+                renderer.Device.SetFillModeDefault();
+                renderer.Device.SetCullModeNone();
+                renderer.Device.SetBlendStateAlphaBlend();
+                renderer.Device.ApplyRasterState();
+                renderer.Device.UpdateAllStates();
+
+                renderData.worldViewProjection = Matrix.Scaling(4) * transformMatrix * renderer.viewProjection;
+
+                renderer.Device.UpdateData(renderer.basicBuffer, renderData);
+                renderer.Device.DeviceContext.VertexShader.SetConstantBuffer(0, renderer.basicBuffer);
+                renderer.basicShader.Apply();
+
+                renderer.Cube.Draw(renderer.Device);
+            }
+        }
+    
         public float Scale
         {
             get => ReadFloat(4);
