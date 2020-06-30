e3bbf110c63ec7dab52e2fdb6881a35a3196b834
Muzzarino <dvdresources@gmail.com>
2020-06-30 14:37:03 -0700

    Update Object1184_SmokeScreen.cs

--- HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesHeroes/List11/Object1184_SmokeScreen.cs
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesHeroes/List11/Object1184_SmokeScreen.cs
@@ -1,4 +1,4 @@
-﻿using SharpDX;
+using SharpDX;
 
 namespace HeroesPowerPlant.LayoutEditor
 {
@@ -10,6 +10,45 @@ namespace HeroesPowerPlant.LayoutEditor
                 DefaultTransformMatrix();
 
             CreateBoundingBox();
+        }
+        
+        public override void Draw(SharpRenderer renderer)
+        {
+            if (models != null && renderer.dffRenderer.DFFModels.ContainsKey(ModelNames[(int)ModelNumber][0]))
+            {
+                SetRendererStates(renderer);
+                renderData.worldViewProjection = transformMatrix * renderer.viewProjection;
+
+                renderer.Device.SetBlendStateAdditive();
+                renderer.Device.SetCullModeNone();
+                renderer.Device.ApplyRasterState();
+                renderer.Device.UpdateAllStates();
+
+                renderer.Device.UpdateData(renderer.tintedBuffer, renderData);
+                renderer.Device.DeviceContext.VertexShader.SetConstantBuffer(0, renderer.tintedBuffer);
+
+                if (renderer.dffRenderer.DFFModels[ModelNames[(int)ModelNumber][0]] != null)
+                    renderer.dffRenderer.DFFModels[ModelNames[(int)ModelNumber][0]].Render(renderer.Device);
+            }
+
+            else
+            {
+                renderData.Color = isSelected ? renderer.selectedColor : renderer.normalColor;
+
+                renderer.Device.SetFillModeDefault();
+                renderer.Device.SetCullModeNone();
+                renderer.Device.SetBlendStateAlphaBlend();
+                renderer.Device.ApplyRasterState();
+                renderer.Device.UpdateAllStates();
+
+                renderData.worldViewProjection = Matrix.Scaling(4) * transformMatrix * renderer.viewProjection;
+
+                renderer.Device.UpdateData(renderer.basicBuffer, renderData);
+                renderer.Device.DeviceContext.VertexShader.SetConstantBuffer(0, renderer.basicBuffer);
+                renderer.basicShader.Apply();
+
+                renderer.Cube.Draw(renderer.Device);
+            }
         }
 
         public int ModelNumber
