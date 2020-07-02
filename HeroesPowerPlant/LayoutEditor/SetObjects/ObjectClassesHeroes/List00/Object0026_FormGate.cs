1f4e905172f81fb46dcfc1034f8adc51b918431f
Muzzarino <dvdresources@gmail.com>
2020-07-01 17:55:34 -0700

    Update Object0026_FormGate.cs

--- HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesHeroes/List00/Object0026_FormGate.cs
+++ HeroesPowerPlant/LayoutEditor/SetObjects/ObjectClassesHeroes/List00/Object0026_FormGate.cs
@@ -2,6 +2,55 @@
 {
     public class Object0026_FormGate : SetObjectHeroes
     {
+        private Vector4 BeamColor;
+        public override void Draw(SharpRenderer renderer)
+        {
+            if (renderer.dffRenderer.DFFModels.ContainsKey("EF_FCHG_BEAM.DFF") | renderer.dffRenderer.DFFModels.ContainsKey("OBJ_SIGNAL_SHADE.DFF"))
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    SetRendererStates(renderer);
+                    renderData.worldViewProjection = Matrix.RotationY(i * MathUtil.Pi) * Matrix.Translation(0f, 0f, i * 20f) * transformMatrix * renderer.viewProjection;
+                    switch ((byte)Formation)
+                    {
+                        case 0: BeamColor = new Vector4(0f, 90f, 170f, 255f); break;
+                        case 1: BeamColor = new Vector4(250f, 165f, 25f, 255f); break;
+                        case 2: BeamColor = new Vector4(240f, 25f, 60f, 255f); break;
+                        default: BeamColor = new Vector4(255f, 255f, 255f, 255f); break;
+                    }
+                    renderData.Color = (isSelected ? renderer.selectedColor : Vector4.One) * (BeamColor / 255f);
+                    renderer.Device.SetBlendStateAdditive();
+                    renderer.Device.SetCullModeNone();
+                    renderer.Device.ApplyRasterState();
+                    renderer.Device.UpdateAllStates();
+
+                    renderer.Device.UpdateData(renderer.tintedBuffer, renderData);
+                    renderer.Device.DeviceContext.VertexShader.SetConstantBuffer(0, renderer.tintedBuffer);
+
+                    if (renderer.dffRenderer.DFFModels.ContainsKey("EF_FCHG_BEAM.DFF"))
+                    {
+                        renderer.dffRenderer.DFFModels["EF_FCHG_BEAM.DFF"].Render(renderer.Device);
+                    }
+                    if (i == 0)
+                    {
+                        renderer.dffRenderer.DFFModels["OBJ_SIGNAL_SHADE.DFF"].Render(renderer.Device);
+                    }
+                }
+            }
+
+            if (renderer.dffRenderer.DFFModels.ContainsKey(ModelNames[0][0]))
+            {
+                SetRendererStates(renderer);
+                renderer.dffRenderer.DFFModels[ModelNames[0][0]].Render(renderer.Device);
+            }
+
+            else
+            {
+                SetRendererStates(renderer);
+                DrawCube(renderer);
+            }
+        }
+        
         public Formation Formation
         {
             get => (Formation)ReadByte(4);
