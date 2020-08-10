c3d0d0e964df4640e111f5e1ac532405be27380c
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2020-08-10 10:18:59 -0300

    .

--- HeroesPowerPlant/Shared/IO/Config/AutomaticUpdater.cs
+++ HeroesPowerPlant/Shared/IO/Config/AutomaticUpdater.cs
@@ -86,8 +86,7 @@ namespace HeroesPowerPlant.Shared.IO.Config
                             if (Directory.Exists(Application.StartupPath + s))
                                 foreach (string s2 in Directory.GetFiles(Application.StartupPath + s))
                                 {
-                                    if ((Path.GetExtension(s2).ToLower().Equals(".zip") && !Path.GetFileName(s2).Equals("Reloaded-Mod-Template.zip")) ||
-                                        Path.GetExtension(s2).ToLower().Equals(".json"))
+                                    if (Path.GetExtension(s2).ToLower().Equals(".zip") || Path.GetFileNameWithoutExtension(s2).Equals("HPPConfig.json"))
                                         continue;
 
                                     string newFilePath = oldPath + s + "\\" + Path.GetFileName(s2);
