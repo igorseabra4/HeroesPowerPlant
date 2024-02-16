60c8a0d7c9158962fb5f15c794df1ccd83e731be
dreamsyntax <dreamsyntax@gmail.com>
2024-02-15 21:30:09 -0700

    Update submodules & dependencies; Version bump

--- HeroesPowerPlant/Shared/IO/Config/HPPVersion.cs
+++ HeroesPowerPlant/Shared/IO/Config/HPPVersion.cs
@@ -2,7 +2,7 @@
 {
     public class HPPVersion
     {
-        public string version = "2024.01.19";
-        public string versionName = "2024.01.19";
+        public string version = "2024.02.15";
+        public string versionName = "2024.02.15";
     }
 }
