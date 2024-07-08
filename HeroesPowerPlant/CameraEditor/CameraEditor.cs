4bcb55603cd14ecb5b1deef327cf3a9f370f8e87
dreamsyntax <dreamsyntax@gmail.com>
2024-07-08 14:10:09 -0700

    add wip shadow->heroes camera import, unhide SA camera import

--- HeroesPowerPlant/CameraEditor/CameraEditor.cs
+++ HeroesPowerPlant/CameraEditor/CameraEditor.cs
@@ -18,10 +18,6 @@ namespace HeroesPowerPlant.CameraEditor
         {
             InitializeComponent();
 
-#if !DEBUG
-            importSACameraFileToolStripMenuItem.Visible = false;
-#endif
-
             numericUpDownCamType.Maximum = Decimal.MaxValue;
             numericUpDownCamSpeed.Maximum = Decimal.MaxValue;
             numericUpDown3.Maximum = Decimal.MaxValue;
@@ -558,6 +554,17 @@ namespace HeroesPowerPlant.CameraEditor
             using VistaOpenFileDialog openFile = new VistaOpenFileDialog();
             if (openFile.ShowDialog() == DialogResult.OK)
                 foreach (var c in Other.OtherFunctions.ConvertSACamToHeroes(openFile.FileName))
+                {
+                    ListBoxCameras.Items.Add(c);
+                    UnsavedChanges = true;
+                }
+        }
+
+        private void importShadowCameraFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using VistaOpenFileDialog openFile = new VistaOpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+                foreach (var c in Other.OtherFunctions.ConvertShadowCamToHeroes(openFile.FileName))
                 {
                     ListBoxCameras.Items.Add(c);
                     UnsavedChanges = true;
