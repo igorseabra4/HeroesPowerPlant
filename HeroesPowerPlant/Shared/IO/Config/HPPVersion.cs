1697475389b83f0ba6123f09b420ccd10d93bcb0
dreamsyntax <dreamsyntax@gmail.com>
2024-07-18 10:45:18 -0700

    Upgrade version

--- HeroesPowerPlant/Shared/IO/Config/HPPVersion.cs
+++ HeroesPowerPlant/Shared/IO/Config/HPPVersion.cs
@@ -2,7 +2,7 @@
 {
     public class HPPVersion
     {
-        public string version = "2024.07.07";
-        public string versionName = "2024.07.07";
+        public string version = "2024.07.18";
+        public string versionName = "2024.07.18";
     }
 }
