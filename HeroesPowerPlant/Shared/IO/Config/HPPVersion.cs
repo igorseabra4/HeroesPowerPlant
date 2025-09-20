e25399114ecd8a817f2b2e81a2b9bf5d76dc832f
dreamsyntax <dreamsyntax@gmail.com>
2025-09-19 22:48:12 -0700

    Set version 2025.09.19

--- HeroesPowerPlant/Shared/IO/Config/HPPVersion.cs
+++ HeroesPowerPlant/Shared/IO/Config/HPPVersion.cs
@@ -2,7 +2,7 @@
 {
     public class HPPVersion
     {
-        public string version = "2025.07.01";
-        public string versionName = "2025.07.01";
+        public string version = "2025.09.19";
+        public string versionName = "2025.09.19";
     }
 }
